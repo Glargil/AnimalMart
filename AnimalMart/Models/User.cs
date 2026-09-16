219ec0640125e40c58edbb830b2dd32797544f4f
TokeDit <tokedit11@gmail.com>
2026-09-16 10:25:30 +0200

    changed properies in User.cs

--- AnimalMart/Models/User.cs
+++ AnimalMart/Models/User.cs
@@ -1,9 +1,9 @@
 public class User
 {
     public int Id { get; private set; }
-    public string? Name { get; set; }
-    public string? Email { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
     public string? PhoneNumber { get; set; }
-    public string? Password { get; private set; }
+    public string PasswordHash { get; private set; }
     public ShoppingCart Cart { get; set; } = new ShoppingCart();
 }
